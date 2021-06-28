32f67dd3fa23529f614232d0c74d1d15a00fd2ef
hinafi <quarkend@live.fr>
2021-06-28 10:27:00 +0200

    shareProfileImg

--- frontend/src/components/share/Share.jsx
+++ frontend/src/components/share/Share.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
 import "./share.css"
+import { PermMedia } from "@material-ui/icons"
 
 
 export default function Share() {
     return (
         <div className="share">
-            <div class="shareWrapper"></div>
+            <div className="shareWrapper">
+                <div className="shareTop"></div>
+                <img className="shareProfileImg" src="/assets/person/21.jpg" alt="" />
+                <input placeholder="whats in your mind?" className="shareInput" />
+            </div>
+            <hr className="shareHr" />
+            <div className="shareBottom">
+                <div class="shareOptions">
+                    <div class="shareOption">
+                        <PermMedia className="shareIcon" />
+                        <span className="shareOptionText">Photo or Video</span>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
