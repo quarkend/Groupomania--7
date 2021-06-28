8441c6d1699426b3192da852cdd64b095a499a29
hinafi <quarkend@live.fr>
2021-06-28 11:29:41 +0200

    share style

--- frontend/src/components/share/Share.jsx
+++ frontend/src/components/share/Share.jsx
@@ -7,16 +7,17 @@ export default function Share() {
     return (
         <div className="share">
             <div className="shareWrapper">
-                <div className="shareTop"></div>
-                <img className="shareProfileImg" src="/assets/person/21.jpg" alt="" />
-                <input placeholder="whats in your mind?" className="shareInput" />
-            </div>
-            <hr className="shareHr" />
-            <div className="shareBottom">
-                <div class="shareOptions">
-                    <div class="shareOption">
-                        <PermMedia className="shareIcon" />
-                        <span className="shareOptionText">Photo or Video</span>
+                <div className="shareTop">
+                    <img className="shareProfileImg" src="/assets/person/21.jpg" alt="" />
+                    <input placeholder="whats in your mind?" className="shareInput" />
+                </div>
+                <hr className="shareHr" />
+                <div className="shareBottom">
+                    <div class="shareOptions">
+                        <div class="shareOption">
+                            <PermMedia className="shareIcon" />
+                            <span className="shareOptionText">Photo or Video</span>
+                        </div>
                     </div>
                 </div>
             </div>
