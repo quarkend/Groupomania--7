6be45b8a181c70d5dc0677f383fba7e5fcf7194c
hinafi <quarkend@live.fr>
2021-06-29 11:03:31 +0200

    dummyData

---
+++ frontend/src/pages/home/dummyData.js
@@ -0,0 +1,36 @@
+export const Users = [
+    {
+        id: 1,
+        profilePicture: "assets/person/6.jpg",
+        username: "Rayen Hinafi",
+    },
+    {
+        id: 2,
+        profilePicture: "assets/person/1.jpg",
+        username: "Raygggen Higggnafi",
+    }
+
+
+];
+export const Posts = [
+    {
+        id: 1,
+        desc: "love for all hartred for none",
+        photo: "assets/person/6.jpg",
+        date: "5 mins a go",
+        userId: 1,
+        like: 322,
+        comment: 1,
+    },
+    {
+        id: 2,
+        desc: "love for all hartred for none",
+        photo: "assets/person/6.jpg",
+        date: "5 mins a go",
+        userId: 1,
+        like: 322,
+        comment: 1,
+    }
+
+
+];
