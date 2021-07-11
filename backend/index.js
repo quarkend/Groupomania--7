883085aff5ca7dab29f7a6cac6e239862b6bd4c1
hinafi <quarkend@live.fr>
2021-07-11 11:52:29 +0200

    auth

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,21 @@ const helmet = require('helmet');
 const dotenv = require('dotenv');
 dotenv.config();
 const app = express();
+const userRoute = require('./routes/users');
+const userRoute = require('./routes/auth');
+
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+// app.get("/", (rep, res) => {
+//     res.send("Welcome to homepage ");
+// });
+// app.get("/users", (rep, res) => {
+//     res.send("Welcome to user ");
+// });
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
 app.listen(8800, () => {
-    console.log("backend server is runing")
-})
+    console.log("backend server is runing");
+});
