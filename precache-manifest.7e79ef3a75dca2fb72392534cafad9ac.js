560a24c6a56822c0b581f25afe081167a170c3bf
quarkend <39971072+quarkend@users.noreply.github.com>
2022-05-22 13:13:43 +0200

    Updates

---
+++ precache-manifest.7e79ef3a75dca2fb72392534cafad9ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6fb588c89a224bb5f62e",
+    "url": "/static/css/main.35266746.chunk.css"
+  },
+  {
+    "revision": "6fb588c89a224bb5f62e",
+    "url": "/static/js/main.6fb588c8.chunk.js"
+  },
+  {
+    "revision": "7d6e47e9718b30d4195d",
+    "url": "/static/js/1.7d6e47e9.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "409305af8e966e8cce7f00476dbc8988",
+    "url": "/index.html"
+  }
+];
