91822354538481a99e5c7f98144c6e3ae6d815ac
quarkend <39971072+quarkend@users.noreply.github.com>
2022-05-22 13:27:56 +0200

    Updates

---
+++ precache-manifest.fd1b5efa3339fa8c142bd20d680fe1e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6fb588c89a224bb5f62e",
+    "url": "/Groupomania--7/static/css/main.35266746.chunk.css"
+  },
+  {
+    "revision": "6fb588c89a224bb5f62e",
+    "url": "/Groupomania--7/static/js/main.6fb588c8.chunk.js"
+  },
+  {
+    "revision": "7d6e47e9718b30d4195d",
+    "url": "/Groupomania--7/static/js/1.7d6e47e9.chunk.js"
+  },
+  {
+    "revision": "2ff0fb4bb81c3ac93783",
+    "url": "/Groupomania--7/static/js/runtime~main.2ff0fb4b.js"
+  },
+  {
+    "revision": "8cd293a07561efc8d68a9407929f65e9",
+    "url": "/Groupomania--7/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7e79ef3a75dca2fb72392534cafad9ac.js"
+  "/Groupomania--7/precache-manifest.fd1b5efa3339fa8c142bd20d680fe1e1.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/Groupomania--7/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
