fbae999e8f77e9c56ce308f374e304ebe2673f6f
quarkend <39971072+quarkend@users.noreply.github.com>
2022-06-02 12:58:28 +0200

    build docs

---
+++ frontend/docs/precache-manifest.9c2ca0906c045676ba7864a0bf3f56f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bad6c49b9a56cdf11316",
+    "url": "/Groupomania--7/static/css/main.28c4d833.chunk.css"
+  },
+  {
+    "revision": "bad6c49b9a56cdf11316",
+    "url": "/Groupomania--7/static/js/main.bad6c49b.chunk.js"
+  },
+  {
+    "revision": "b597ea6641f2b53ca576",
+    "url": "/Groupomania--7/static/js/1.b597ea66.chunk.js"
+  },
+  {
+    "revision": "2ff0fb4bb81c3ac93783",
+    "url": "/Groupomania--7/static/js/runtime~main.2ff0fb4b.js"
+  },
+  {
+    "revision": "7a784a5b2c6dc24f1e3c8c036c31be27",
+    "url": "/Groupomania--7/index.html"
+  }
+];
